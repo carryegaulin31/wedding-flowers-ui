8466e858fae205008f9a08c9256fdbc0249a61e9
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-10 13:38:46 -0500

    controllers folder and flowers,colors and seasons file create

--- components/models/flowers.js
+++ components/models/flowers.js
@@ -1,5 +1,5 @@
 export default (connection, Sequelize, seasons) => connection.define('flowers', {
   id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
   name: { type: Sequelize.STRING, allowNull: false },
-  seasonId: { type: Sequelize.INTEGER, references: { model: Authors, key: 'id' } },
+  seasonId: { type: Sequelize.INTEGER, references: { model: seasons, key: 'id' } },
 })
