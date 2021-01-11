72ea21f3ebdf5d2bbb84695e8d2ff5eba57cd9dc
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-10 21:26:57 -0500

    use state add to flowers.jsx

--- components/Flowers.jsx
+++ components/Flowers.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-export default () => (
-  <div>
-    <div>Wedding Flowers</div>
-    <div>A searchable list of flowers to make your wedding perfect</div>
-  </div>
-)
+export default () => {
+  const [name, setName] = useState('')
+
+  return (
+    <div className="page">
+      <div className="title">Wedding Flowers UI</div>
+      <div className="subtitle">A searchable list of flowers to make your wedding perfect</div>
+      <input type="text" name="search" />
+      <div className="name">name</div>
+    </div>
+  )
+}
