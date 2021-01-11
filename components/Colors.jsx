7d29576ecf291423c8a775141a0fc26966ec449a
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-11 00:00:21 -0500

    filteredColorList add in Colors.jsx

--- components/Colors.jsx
+++ components/Colors.jsx
@@ -4,25 +4,31 @@ import Color from './Color'
 import Search from './Search'
 
 export default () => {
-  const [name, setName] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
   const [colorList, setColorList] = useState([])
-
+  const [filteredColorList, setFilteredColorList] = useState([])
   useEffect(() => {
     async function pullData() {
-      const { data } = await axios.get('http://localhost:1337/api/flowers')
+      const { data } = await axios.get('http://localhost:1337/api/colors')
 
       setColorList(data)
+      setFilteredColorList(data)
     }
 
     pullData()
   }, [])
+
+  useEffect(() => {
+
+  }, [searchTerm])
+
   return (
     <div className="page">
       <div className="title">Wedding Flowers UI</div>
       <div className="subtitle">A searchable list of flowers by color to make your wedding perfect</div>
-      <Search term={name} setter={setName} />
+      <Search term={searchTerm} setter={setSearchTerm} />
       {
-        colorList.map(color => (<Color key={color.id} id={color.id} name={color.name} />))
+        filteredColorList.map(color => (<Color key={color.id} id={color.id} name={color.name} />))
       }
     </div>
   )
