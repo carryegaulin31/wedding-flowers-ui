4b56d25b9fc6ace5fc838bc02c8795959fed39e7
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-10 20:40:10 -0500

    server.js update

--- public/javascript/bundle.js
+++ public/javascript/bundle.js
@@ -16,7 +16,7 @@
   \********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ \"./node_modules/react-dom/index.js\");\n/* harmony import */ var _components_Flowers__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/Flowers */ \"./components/Flowers.jsx\");\n\n\n\n(0,react_dom__WEBPACK_IMPORTED_MODULE_1__.render)( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"div\", null, \"Flowers\"), document.getElementById('root'));\n\n//# sourceURL=webpack://wedding-flowers-ui/./client.jsx?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ \"./node_modules/react-dom/index.js\");\n/* harmony import */ var _components_Flowers__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/Flowers */ \"./components/Flowers.jsx\");\n\n\n\n(0,react_dom__WEBPACK_IMPORTED_MODULE_1__.render)( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_components_Flowers__WEBPACK_IMPORTED_MODULE_2__.default, null), document.getElementById('root'));\n\n//# sourceURL=webpack://wedding-flowers-ui/./client.jsx?");
 
 /***/ }),
 
--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 import express from 'express'
-import bodyParser from 'body-parser'
+// import bodyParser from 'body-parser'
 import path from 'path'
+import { getAllColors, getColorByNameOrId } from './controllers/colors'
+import { getAllFlowers, getFlowerByNameOrId } from './controllers/flowers'
+import { getAllSeasons, getSeasonByName } from './controllers/seasons'
 
 const app = express()
 
 app.use(express.static('public'))
 
+app.get('/api/seasons', getAllSeasons)
+app.get('/api/seasons/:name', getSeasonByName)
+
+app.get('/api/colors', getAllColors)
+app.get('/api/colors/:name', getColorByNameOrId)
+
+app.get('/api/flowers', getAllFlowers)
+app.get('/api/flowers/:name', getFlowerByNameOrId)
+
 app.all('*', (request, response) => response.sendFile(path.resolve(__dirname, 'public', 'index.html')))
+
 app.listen(1337, () => {
-  console.log('Listening on port 1337...')
+  console.log('Listening on port 1337...') // eslint-disable-line no-console
+})
