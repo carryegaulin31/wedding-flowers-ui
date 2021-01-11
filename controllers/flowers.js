bdaf8d312a95e261ace330f13289a5e3a7761fdc
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-10 23:16:21 -0500

    saveNewFlower...need to figure out color and season add with tables

--- controllers/flowers.js
+++ controllers/flowers.js
@@ -25,3 +25,22 @@ export const getFlowerByName = async (request, response) => {
     ? response.send(flower)
     : response.sendStatus(404)
 }
+
+export const saveNewFlower = async (request, response) => {
+  try {
+    const { name } = request.body
+
+    if (!name) {
+      return response
+        .status(400)
+        .send('The following fields are required: name')
+    }
+
+    const newFlower = await models.heroes.create({
+      name })
+
+    return response.status(201).send(newFlower)
+  } catch (error) {
+    return response.status(500).send('Unable to save new flower, please try again')
+  }
+}
