42e6880dac24b89ca4fb9c423e25b123ca0467b1
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-14 12:32:06 -0500

    changed colors to colorsPage to reflect the move"

--- Client.jsx
+++ Client.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Colors from './components/Colors'
+import ColorsPage from './pages/Colors'
 
 render(
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={Colors} />
+      <Route exact path="/" component={ColorsPage} />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root'),
