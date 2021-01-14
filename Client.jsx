303c0164c03cf3ab6183bdcd409e33ca908a2ae7
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-14 13:21:33 -0500

    Flowers page create and import to client. Flowers Page info update

--- Client.jsx
+++ Client.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import ErrorPage from './pages/Error'
+import FlowersPage from './pages/Flowers'
 import ColorsPage from './pages/Colors'
 
 render(
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={ColorsPage} />
+      <Route exact path="/flowers" component={FlowersPage} />
       <Route path="*" component={ErrorPage} />
     </Switch>
   </BrowserRouter>,
