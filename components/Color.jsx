362629a82e34f3f727ca087fe6bf6ccbc167b163
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-14 13:10:46 -0500

    color component  CSS add

--- components/Color.jsx
+++ components/Color.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const Team = styled.div`
+  font-size: 20px;
+  margin-bottom: 10px;
+`
 export default ({ id, name }) => (
   <div key={id}>{`${name}`}</div>
 )
