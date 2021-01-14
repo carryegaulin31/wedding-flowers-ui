5c76d668a8b2e22541fc42226f816053db7b4562
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-14 13:07:10 -0500

    Title component update with Title & Subtitle div. CSS add

--- components/Title.jsx
+++ components/Title.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
+import styled from 'styled-components'
+
+const Title = styled.div`
+  font-size: 24px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`
+
+const Subtitle = styled.div`
+  font-size: 20px;
+`
+
+
 export default () => (
   <>
-    <div className="title">Wedding Flowers Source</div>
-    <div className="subtitle">A searchable list of flowers by color to make your wedding perfect</div>
+    <Title>Wedding Flowers Source</Title>
+    <Subtitle>A searchable list of flowers by color to make your wedding perfect</Subtitle>
   </>
 )
