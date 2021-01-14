86db617b455465bb81e5f8a9c36db32947a063a6
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-14 13:43:47 -0500

    flowers in the utils folder update with if conditions to return the flowers for colors

--- utils/flowers.js
+++ utils/flowers.js
@@ -1,3 +1,16 @@
-export const retrieveFlowers = async (location) => {
+export const getColorNameFromUrl = location => (location && location.pathname
+  ? location.pathname.split('/heroes/').pop()
+  : ''
+)
 
+export const retrieveFlowers = async (location) => {
+  const colorName = getColorNameFromUrl(location)
+
+  if (!colorName) return { details: {}, flowers: [] }
+
+  const { id, name, flowers } = await fetchFlowersForColors(colorName)
+
+  if (!id || !name || !flowers) return { details: {}, flowers: [] }
+
+  return { flowers, details: { id, name } }
 }
