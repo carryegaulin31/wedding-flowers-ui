699e18d1b9d2e75e1bffe8fd61a8d077088fe91e
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-01-21 17:49:24 -0500

    flowersDetails page add update, flowers actions update, flowers utils update

--- actions/flowers.js
+++ actions/flowers.js
@@ -1,7 +1,13 @@
 import axios from 'axios'
 
-export default async (name) => {
-  const { data } = await axios.get(`${API_BASE_URL}/colors/${name}`) // eslint-disable-line no-undef
+export const fetchColors = async () => {
+  const { data } = await axios.get('http://localhost:1337/api/flowers')
+
+  return data
+}
+
+export const fetchColorAndFlowers = async (name) => {
+  const { data } = await axios.get(`${API_BASE_URL}/flowers/${name}`) // eslint-disable-line no-undef
 
   return data
 }
--- utils/flowers.js
+++ utils/flowers.js
@@ -1,1 +1,25 @@
-import fetchFlowersForColors from '../actions/flowers'
+import { fetchFlowers, fetchFlowerAndColors } from '../actions/flowers'
+
+export const getFlowerNameFromUrl = location => (location && location.pathname
+  ? location.pathname.split('/flowers/').pop()
+  : ''
+)
+export const filterColors = (list, term) => list.filter(flowerr => flower.name.toLowerCase().includes(term.toLowerCase()))
+
+export const retrieveFlowers = async () => {
+  const colors = await fetchFlowers()
+
+  return colors
+}
+
+export const retrieveFlowerWithAssociatedColors = async (location) => {
+  const flowerName = getFlowerNameFromUrl(location)
+
+  if (!flowerName) return { details: {}, colors: [] }
+
+  const { id, name, colors } = await fetchFlowerAndColors(flowerName)
+
+  if (!id || !name || !colors) return { details: {}, colors: [] }
+
+  return { colors, details: { id, name } }
+}
